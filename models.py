f597c330f506e10c312a537938de8f14140ab01d
ivan-os07 <ivan07-os@yandex.ru>
2025-10-15 09:54:18 +0500

    add relationship with model

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import uuid
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Uuid
+from sqlalchemy import Uuid, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 from sqlalchemy import String
 
@@ -17,8 +17,18 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(50), unique=True)
     tg_id: Mapped[str] = mapped_column(unique=True)
 
+    todos: Mapped[list["Todo"]] = relationship(
+        "Todo", back_populates="user", cascade="all, delete-orphan"
+    )
+
 
 class Todo(Base):
     __tablename__ = "todos"
-    
+
     id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
+
+    # Внешний ключ на User.id
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    # Обратная ссылка
+    user: Mapped["User"] = relationship("User", back_populates="todos")
